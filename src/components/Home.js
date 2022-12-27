0847d7efa55943175e85ccbd319fc4a808622482
Raj Pal <rpal778866@gmail.com>
2022-12-27 17:28:16 +0530

    few

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
           <p className="text-gray-500 py-4 max-w-md">
             I have 2+ years of experience building and desgining software.
             Currently, I love to work on web application using technologies like
-            React JS , Redux , Javascript and Tailwind .
+            <strong> React JS , Redux , Javascript and Tailwind css. </strong>
           </p>
 
           <div>
